878a05171c79f887923974c23f93756330d95316
AlphaNull <58626192+AlphaNull16299@users.noreply.github.com>
2022-02-23 00:51:39 +0900

    http serverを建ててみｔ

--- index.go
+++ index.go
@@ -1,31 +1,7 @@
-package main
+package main // mainパッケージであることを宣言
 
-import "fmt"
-import "os"
+import "fmt" // fmtモジュールをインポート
 
-func main() {
-	var a, b, c int
-	var calc string
-
-	fmt.Printf("<Formula>\n")
-	fmt.Printf("ex) 1 + 1 = ? -> 1 1 +\n")
-	fmt.Scanf("%d %d %s", &a, &b, &calc)
-
-	if calc == "+" {
-		c = a + b
-	} else if calc == "-" {
-		c = a - b
-	} else if calc == "*" {
-		c = a * b
-	} else if calc == "/" {
-		if b == 0 {
-			fmt.Printf("I cannot calculate this formula.\n")
-			os.Exit(1)
-		} else {
-			c = a / b
-		}
-	}
-
-	fmt.Printf("<Answer>\n")
-	fmt.Printf("%d %s %d = %d", a, calc, b, c)
+func main() { // 最初に実行されるmain()関数を定義
+	fmt.Println("hello, world")
 }
